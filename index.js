41d81212f0d099d6c633a5d62765bb125b90fe99
nirajgeorgian <nirajgeorgian@gmail.com>
2018-08-23 02:30:43 +0530

    added users creation and fetching

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const mongoose = require('mongoose')
 const router = express.Router()
 
 const app = express()
+app.use(cors())
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 mongoose.connect("mongodb://127.0.0.1:27017/dodo", {useNewUrlParser: true })
 	.then(res => {
 		console.log("connected to database")
@@ -23,13 +26,60 @@ const personSchema = new mongoose.Schema({
 
 const Person = mongoose.model('Person', personSchema)
 
-app.use(cors())
 router.route("/")
   .get((req, res) => {
     res.send({
       dodo: "duck"
     })
   })
+
+/*
+	Utility merror message
+*/
+const checkKeys = (obj, ...args)	=> {
+	let value = true
+	const keys = Object.keys(obj)
+	args.forEach(key => {
+		value = value && keys.includes(key)
+	})
+	return value
+}
+
+const errMessage = (res, msg) => {
+	return res.status(401).send({
+		status: false,
+		msg
+	})
+}
+
+const succMessage = (res, data) => {
+	return res.status(200).send({
+		status: true,
+		data
+	})
+}
+
+router.route("/users")
+	.post((req, res) => {
+		if(checkKeys({...req.body}, "name", "email", "phone")) {
+			const newPerson = new Person({...req.body })
+			newPerson.save()
+				.then(person => {
+					succMessage(res, person)
+				})
+		} else {
+			errMessage(res, "please pass all parameter")
+		}
+	})
+	.get((req, res) => {
+		Person.find({})
+			.then(persons => {
+				succMessage(res, persons)
+			})
+			.catch(err => {
+				return errMessage(res, err.message())
+			})
+	})
 
 app.use("/", router)
 
